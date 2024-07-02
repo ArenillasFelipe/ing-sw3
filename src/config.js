6a30eae25306fd553b3a749d3afa948d5434b902
ArenillasFelipe <felipearenillas02@gmail.com>
2024-07-02 16:54:48 -0300

    asddd

--- src/config.js
+++ src/config.js
@@ -3,12 +3,12 @@ import { config } from "dotenv";
 config();
 
 
-export const PORT = process.env.PORT || 3000
-export const DB_USER = process.env.DB_USER || 'root'
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'root'
-export const DB_HOST = process.env.DB_HOST || 'localhost'
-export const DB_DATABASE = process.env.DB_DATABASE || 'employeesdb'
-export const DB_PORT = process.env.DB_PORT || 3306
+export const PORT = process.env.PORT
+export const DB_USER = process.env.DB_USER
+export const DB_PASSWORD = process.env.DB_PASSWORD
+export const DB_HOST = process.env.DB_HOST
+export const DB_DATABASE = process.env.DB_DATABASE
+export const DB_PORT = process.env.DB_PORT
 
 
 console.log("DB parameters ", DB_USER, DB_PORT, DB_PASSWORD, DB_DATABASE, DB_HOST);
