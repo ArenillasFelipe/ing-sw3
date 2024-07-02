2f138f3912eb35844fa4839e07d918071848b350
ArenillasFelipe <felipearenillas02@gmail.com>
2024-07-02 16:58:03 -0300

    asdddd

--- src/config.js
+++ src/config.js
@@ -3,12 +3,12 @@ import { config } from "dotenv";
 config();
 
 
-export const PORT = process.env.PORT
-export const DB_USER = process.env.DB_USER
-export const DB_PASSWORD = process.env.DB_PASSWORD
-export const DB_HOST = process.env.DB_HOST
-export const DB_DATABASE = process.env.DB_DATABASE
-export const DB_PORT = process.env.DB_PORT
+export const PORT = process.env.PORT || 3000
+export const DB_USER = 'root'
+export const DB_PASSWORD = 'zkYcYtbuNjefhMidWJQYoTLTGuXvTuOJ'
+export const DB_HOST = 'monorail.proxy.rlwy.net'
+export const DB_DATABASE = 'railway'
+export const DB_PORT = '25785'
 
 
 console.log("DB parameters ", DB_USER, DB_PORT, DB_PASSWORD, DB_DATABASE, DB_HOST);
