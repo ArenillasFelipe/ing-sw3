a39513185063a1c88116d9b9716331ac90a25dd5
ArenillasFelipe <felipearenillas02@gmail.com>
2024-07-23 18:11:33 -0300

    nose

--- backend/src/config.js
+++ backend/src/config.js
@@ -4,11 +4,11 @@ config();
 
 
 export const PORT = process.env.PORT || 3000
-export const DB_USER = 'root'
-export const DB_PASSWORD = 'zkYcYtbuNjefhMidWJQYoTLTGuXvTuOJ'
-export const DB_HOST = 'monorail.proxy.rlwy.net'
-export const DB_DATABASE = 'railway'
-export const DB_PORT = '25785'
+export const DB_USER = process.env.PORT || 'root'
+export const DB_PASSWORD = process.env.PORT || 'zkYcYtbuNjefhMidWJQYoTLTGuXvTuOJ'
+export const DB_HOST = process.env.PORT || 'monorail.proxy.rlwy.net'
+export const DB_DATABASE = process.env.PORT || 'railway'
+export const DB_PORT = process.env.PORT || '25785'
 
 
 console.log("DB parameters ", DB_USER, DB_PORT, DB_PASSWORD, DB_DATABASE, DB_HOST);
